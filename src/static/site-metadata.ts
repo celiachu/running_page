7029f03322702e866b4a637b33b7b2bcaa542de8
Celiachu <306895430@qq.com>
2025-12-01 14:33:49 +0800

    feat:init

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,8 +15,8 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'Celiachu\'s Running Page',
+  siteUrl: 'https://celiachu.run',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
@@ -26,11 +26,11 @@ const data: ISiteMetadataResult = {
     },
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://celiachu.github.io',
     },
     {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'Weekly',
+      url: 'https://celiachu.github.io/series/weekly-report',
     },
   ],
 };
