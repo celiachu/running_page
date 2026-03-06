deba099fa6ff1700cf526d3954d1ceaa0f46aaa7
celiachu <306895430@qq.com>
2026-03-06 19:04:53 +0800

    fix: update site URL and change privacy mode to false by default

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,7 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Celiachu\'s Running Page',
-  siteUrl: 'https://celiachu.run',
+  siteUrl: 'https://running.lyberti.site/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
